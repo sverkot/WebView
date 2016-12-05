56b2913ce03cd9ce060fe5c22edb8b48ee15949f
sverkot <sverkot@gmail.com>
2016-12-05 09:29:21 -0600

    Initial commit

---
+++ app/src/main/java/edu/lewisu/cs/sangeetha/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.lewisu.cs.sangeetha.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get a reference to the web view to get ref to a url
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://cs.lewisu.edu/~sangeethamverkot/PAD_Week4_Weather/");
+
+    }
+}
